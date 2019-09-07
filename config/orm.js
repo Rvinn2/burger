8a50b1f47bbdfbe482c98bff6b514a985bc0e8c1
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-09-07 01:07:31 -0400

    updating Orm.js file

--- config/orm.js
+++ config/orm.js
@@ -17,8 +17,8 @@ function objToSql(ob) {
         var value = ob[key];  
     
  if (Object.hasOwnProperty.call(ob, key)) {
-    // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-if (typeof value === "string" && value.indexOf(" ") >= 0) {
+
+    if (typeof value === "string" && value.indexOf(" ") >= 0) {
   value = "'" + value + "'";
   }
   arr.push(key + "=" + value);
