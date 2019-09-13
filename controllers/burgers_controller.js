55bdab894d129520634187e5ef4a459dd1801df0
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-09-13 17:50:04 -0400

    burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,17 +7,17 @@ var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
     burger.all(function(data) {
-      var hbsObject = {
-        burger: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+    //   var hbsObject = {
+    //     burger: data
+    //   };
+      console.log(data);
+      res.render("index",{burger: data});
     });
   });
 
   router.post("/api/burgers", function(req, res) {
-    cat.create([
-      "name", "devoured"
+    burger.create([
+      "name_", "devoured"
     ], [
       req.body.name, req.body.devoured
     ], function(result) {
